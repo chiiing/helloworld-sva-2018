42c43682991915bf7c794f9f6bec4049a93d44f5
chiiing <ctsai6@sva.edu>
2018-10-03 14:46:56 -0400

    hello

--- week4/homework_turtle.py
+++ week4/homework_turtle.py
@@ -2,24 +2,27 @@ from turtle import *
 import turtle 
 
 t = turtle.Turtle()
-
+turtle.screensize(960, 720, "black")
 t.width(1)
 
 t.speed(0)
 
 for x in range(300):
 
-	if x % 4 == 0:
+	if x % 5 == 0:
 		t.color("cadetblue")
 
-	if x % 4 == 1:
+	if x % 5 == 1:
 		t.color("lightsteelblue")
 
-	if x % 4 == 2:
+	if x % 5 == 2:
 		t.color("powderblue")
 
-	if x % 4 == 3:
+	if x % 5 == 3:
 		t.color("mediumaquamarine")
+
+	if x % 5 == 4:
+		t.color("aquamarine")
 
 	t.forward(x * 2)
 	t.left(89)
